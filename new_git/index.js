99f0559db70ccd4f2f016a3b36dcbaba4a262491
Kiran Manne <kiranmanne10011@gmail.com>
2023-09-14 14:45:28 +0530

    kiran

---
+++ new_git/index.js
@@ -0,0 +1,7 @@
+console.log("welcome to javaScript");
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/KiranManne1011/my_js.git
+// git push -u origin main
